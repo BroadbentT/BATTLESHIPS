0f4be9c4b6b45e6922028cfa5d1e1ca7cef2f75d
Terence Broadbent <44728186+BroadbentT@users.noreply.github.com>
2019-07-27 10:29:55 +0100

    Update Ships.java

--- Ships.java
+++ Ships.java
@@ -1,19 +1,39 @@
 // *****************************************************************************
-//
-//  JAVA PROGRAMMER TERENCE BROADBENT - B028035c@student.staffs.ac.uk
-//  DATE: 03.03.2014 VERSION 1.0
-//
+//                 JAVA BATTLESHIPS - YOU SUNK MY BATTLESHIP
+//           BY TERENCE BROADBENT BSC CYBER SECURITY (FIRST CLASS)
+// *****************************************************************************
+
+// ***************************************************************************** 
+// AUTHOR  : Terence Broadbent                                                    
+// CONTRACT: GitHub                                                               
+// Version : 1.0                                                                
+// Details : Load required imports.
+// Modified: N/A
+// *****************************************************************************
+
+package BattleShipsProj;
+
+// ***************************************************************************** 
+// AUTHOR  : Terence Broadbent                                                    
+// CONTRACT: GitHub                                                               
+// Version : 1.0                                                                
+// Details : Ship variables.
+// Modified: N/A
 // *****************************************************************************
 
 public class Ships {
 private final String[]Ship = new String [13];
+
 public void setShip(int Loc, String StringX){
 Ship[Loc] = StringX;} 
+
 public void BShipData(Ships StringX, String StringY){
 for (int NumLoop = 0; NumLoop < 13; NumLoop++){
 StringX.setShip(NumLoop, StringY);}}
+
 public void setBShip(Ships StringX, int NumY){
 StringX.setShip(NumY, "1");}    
+
 public String getBShips(int Loc){
 return Ship[Loc];} 
 }
